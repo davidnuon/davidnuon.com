53dde2ab469532a8e9a160d4f1a5e317d0b705ec
David Nuon <davidnuongm@gmail.com>
2010-12-29 21:38:56 -0800

    initial commit

---
+++ generate.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+from jinja2 import Environment, PackageLoader
+import models
+import os
+env = Environment(loader=PackageLoader('models', 'templates'))
+
+OUTPUT_DIR = './_output/'
+
+template = env.get_template('blog-entry.html')
+resume = env.get_template('resume.html')
+portfolio = env.get_template('portfolio.html')
+about = env.get_template('about.html')
+labs = env.get_template('labs.html')
+
+with open(os.path.join(OUTPUT_DIR, 'resume.html'), 'w') as html:
+    html.write(resume.render())
+    
+with open(os.path.join(OUTPUT_DIR, 'portfolio.html'), 'w') as html:
+    html.write(portfolio.render())
+    
+with open(os.path.join(OUTPUT_DIR, 'about.html'), 'w') as html:
+    html.write(about.render())
+    
+with open(os.path.join(OUTPUT_DIR, 'lab.html'), 'w') as html:
+    html.write(labs.render())
+
+articles = []
+for n in models.posts:
+    condition = False
+
+    articles.append({
+        'title'   : n["data"].title,
+        'url'     : n["data"].url,
+        'current' : condition
+    })
+    
+for post in models.posts:
+    g = post["data"].spit_dict()
+    a = list(articles)
+    
+    for x in a:
+        i = a.index(x)
+    
+        if x["url"] == g["url"]:
+            a[i]["current"] = True
+
+        else:
+            a[i]["current"] = False
+    
+    g["posts"] = a
+                
+    with open(os.path.join(OUTPUT_DIR, g["url"]), 'w') as html:
+        html.write(template.render(g))
+        
+    if models.posts[len(models.posts)-1] == post:
+        with open(os.path.join(OUTPUT_DIR, 'index.html'), 'w') as html:
+                html.write(template.render(g))
---
+++ models.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+from pyquery import PyQuery as pq
+import glob
+
+class Post:
+    title    = ''
+    url      = ''
+    date     = ''
+    content  = ''
+    imageurl = ''
+    extracss = ''
+    extrajs  = ''
+    
+    def spit_dict(self):
+        return {
+            'title':self.title,
+            'url':self.url,
+            'date':self.date,
+            'content':self.content,
+            'imageurl':self.imageurl,
+            'extracss':self.extracss,
+            'extrajs':self.extrajs
+        }
+
+post_files = []
+posts = []
+
+with open('./blogxml/slugs.txt', 'r') as slugs_txt:
+    st = slugs_txt.read().split('\n')
+    for n in st:
+        post_files.append(n.split('|'))
+
+for n in post_files:
+    
+    with open('./blogxml/' + n[0], 'r') as f:
+        d = pq(f.read())
+        g = Post()
+        
+        g.url      =  n[1]
+        g.title    =  d.find('title').html()
+        g.date     =  n[2]
+        g.content  =  d.find('content').html()
+        g.imageurl =  d.find('imageurl').html()
+        g.extracss =  d.find('extracss').html()
+        g.extrajs  =  d.find('extrajs').html()
+        
+        if g.extrajs == None:
+            g.extrajs = ''
+        
+        if n[2] == '':
+            index = False
+        else:
+            index = True
+        
+        posts.append({'data':g, 'index':index })
+        
